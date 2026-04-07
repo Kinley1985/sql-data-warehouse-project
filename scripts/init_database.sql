dc81366bed4b455f26cc8ed7bebeb951f49e4718
Kinley <kinleymerenciano@gmail.com>
2026-04-07 15:52:05 +0800

    init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -2,46 +2,43 @@
 ====================================================================
 Create Database and Schemas
 ====================================================================
-Sript Purpose:
-    This script creates a new database named 'DataWarehouse' after
-    checking if ti already ecists. If the databse exists, it is 
-    dropped and recreated. Additionally, the script sets up three
-    schemas within the database: 'bronze', 'silver', and 'gold'.
+Script Purpose:
+    This script creates a new database named 'DataWarehouse'.
+    If the database already exists, it will be dropped and recreated.
+    The script also creates three schemas: bronze, silver, and gold.
 ====================================================================
 Script: init_database.sql
-    Description: Initialize Data Warehouse database and schemas
-    Author: Kinley Merenciano (inspired by Baraa)
-    Date: 2026
+Description: Initialize Data Warehouse database and schemas
+Author: Kinley Merenciano (inspired by Baraa)
+Date: 2026
 ====================================================================
 WARNING:
     Running this script will drop the entire 'DataWarehouse' database 
-    if it exists. All data in the database will be permanently deleted.
-    Proceed with caution and ensure have proper backups before running
-    the script.
+    if it exists. All data will be permanently deleted.
+    Proceed with caution and ensure you have proper backups.
 ====================================================================
 */
 
 USE master;
 GO
 
-
--- Drop and recreate the 'DataWarehouse' database
-IF NOT EXISTS (SELECT 1 name FROM sys.databases WHERE name = 'DataWarehouse')
+-- Drop database if it exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
     ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE Datawarehouse
+    DROP DATABASE DataWarehouse;
 END;
 GO
 
--- Create the 'DataWarehouse' database
+-- Create database
 CREATE DATABASE DataWarehouse;
 GO
 
--- Use Database
+-- Use database
 USE DataWarehouse;
 GO
 
--- Create Schemas
+-- Create schemas
 CREATE SCHEMA bronze;
 GO
 
